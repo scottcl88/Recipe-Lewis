efb06e3bc6c23e4ab8f8ecb47fa31c02bb7870f1
Scott Lewis <scottcl@outlook.com>
2020-12-29 19:14:19 -0500

    Overriding all identity pages

---
+++ RecipeLewis/Areas/Identity/IdentityHostingStartup.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using RecipeLewis.Data;
+
+[assembly: HostingStartup(typeof(RecipeLewis.Areas.Identity.IdentityHostingStartup))]
+namespace RecipeLewis.Areas.Identity
+{
+    public class IdentityHostingStartup : IHostingStartup
+    {
+        public void Configure(IWebHostBuilder builder)
+        {
+            builder.ConfigureServices((context, services) => {
+            });
+        }
+    }
+}
