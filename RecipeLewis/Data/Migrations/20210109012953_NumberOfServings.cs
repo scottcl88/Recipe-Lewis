361ef9c0d69132804a0b271df56c9363016d9559
Scott Lewis <scottcl@outlook.com>
2021-01-08 20:30:13 -0500

    Add migration NumberOfServings

---
+++ RecipeLewis/Data/Migrations/20210109012953_NumberOfServings.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace RecipeLewis.Data.Migrations
+{
+    public partial class NumberOfServings : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "NumberOfServings",
+                table: "Recipes",
+                type: "nvarchar(max)",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "NumberOfServings",
+                table: "Recipes");
+        }
+    }
+}
